b415f236b7bcae7df7e9398cec723f4b7cfdafbc
Guilherme Victal <guilherme.a@dasa.com.br>
2022-03-11 13:46:43 -0300

    feat: add commitlint check to avoid running commitizen on valid messages

--- hooks/prepare-commit-msg.d/commitizen.sh
+++ hooks/prepare-commit-msg.d/commitizen.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env sh
-echo "$1"
+set -x
+commitlint  -x "$(npm root -g)/@commitlint/config-conventional" -e
+if type commitlint >/dev/null 2>&1;
+then
+    if commitlint  -x "$(npm root -g)/@commitlint/config-conventional" -q -e;
+    then
+        echo "Commit message already matches lint rules. Skipping commitizen prompt."
+        exit 0
+    fi
+fi
 
 if type git-cz >/dev/null 2>&1;
 then
     exec < /dev/tty && npx cz --hook || true
 else
-    echo "Commitizen not found! Install it using `npm install -g commitizen to use this hook`"
+    echo "Commitizen not found! Install it using 'npm install -g commitizen to use this hook'"
 fi
