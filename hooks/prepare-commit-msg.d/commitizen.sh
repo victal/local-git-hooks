7c40bd807a2b044cfb14afc7d111e7b65513a5d8
Guilherme Victal <guilherme.a@dasa.com.br>
2022-03-11 10:56:46 -0300

    fix: add check if commitizen is installed before trying to use it

--- hooks/prepare-commit-msg.d/commitizen.sh
+++ hooks/prepare-commit-msg.d/commitizen.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
-echo "commitizen"
-echo "$@"
+echo "$1"
 
-exec < /dev/tty && npx cz --hook || true
+if type git-cz >/dev/null 2>&1;
+then
+    exec < /dev/tty && npx cz --hook || true
+else
+    echo "Commitizen not found! Install it using `npm install -g commitizen to use this hook`"
+fi
